ba5a3f95adeccfa070d70bcd9d074a26cf26289a
tuhinsherlock <tuhin.loves.federer@gmail.com>
2016-09-23 22:47:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 app.get('/ui/Batman.gif', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Batman.gif'));
 });
