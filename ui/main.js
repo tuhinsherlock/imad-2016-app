86301ff66605cf9d75043ef52ff62615d217fbf5
tuhinsherlock <tuhin.loves.federer@gmail.com>
2016-09-28 20:26:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,5 +10,6 @@ function moveRight () {
 }
 
 img.onclick = function() {
-    var interval = setInterval(moveRight, 100);
+    //var interval = setInterval(moveRight, 100);
+    img.style.marginLeft = '200px'
 };
