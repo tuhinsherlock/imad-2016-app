e0ddd82e01b0713a718dbe79b34ce37ae4151484
tuhinsherlock <tuhin.loves.federer@gmail.com>
2016-10-01 18:25:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,2 @@
 console.log('Loaded!');
 
-var img = document.getElementById('hell');
-
-var marginLeft = 0;
-
-function moveRight () {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick = function() {
-    var interval = setInterval(moveRight, 100);
-    //img.style.marginLeft = '200px'
-};
