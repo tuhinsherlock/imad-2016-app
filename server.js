c2dffe893f10a40ff414ee7ef17de84aa83da15e
tuhinsherlock <tuhin.loves.federer@gmail.com>
2016-10-01 18:32:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res) {
 var articleName = req.params.articleName;
   res.send(createTemplate(stuff[articleName]));
@@ -114,12 +121,7 @@ app.get('/ui/batlogo.gif', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'batlogo.gif'));
 });
 
-var counter=0;
 
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-  res.send(counter.toString());
-});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
