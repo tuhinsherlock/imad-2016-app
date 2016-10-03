39376fe410a9d917c89759ab49960dab4e04f9db
tuhinsherlock <tuhin.loves.federer@gmail.com>
2016-10-04 02:04:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,16 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick= function(){
+     var request = new XMLHttpRequest();
     
-    var names=["name1","name2","name3","name4"];
+    request.onreadystatechange = function() {
+        
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var names= request.responseText;
+                names=JSON.parse(names);
     var list='';
     for(var i=0;i<names.length;i++)
     {
@@ -34,4 +42,10 @@ submit.onclick= function(){
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+            }
+        }
+    };
+    request.open('GET','http://tuhinsherlock.imad.hasura-app.io/submit-name?name='+name, true);
+    request.send(null);
+    
 };
