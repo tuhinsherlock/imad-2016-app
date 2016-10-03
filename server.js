9fb2803e07fee2b8b1717d87ab560197f14a2e80
tuhinsherlock <tuhin.loves.federer@gmail.com>
2016-10-04 01:42:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,8 +123,8 @@ app.get('/ui/batlogo.gif', function (req, res) {
 
 var names=[];
 
-app.get('/submit-name/:name', function(req,res){
-    var name = req.params.name;
+app.get('/submit-name/', function(req,res){
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
