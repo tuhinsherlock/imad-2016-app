dad0b357cf3c33617272bf5ad36ca6d0eb896a0d
tuhinsherlock <tuhin.loves.federer@gmail.com>
2016-10-04 00:44:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,17 @@ button.onclick = function() {
     request.open('GET','http://tuhinsherlock.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+var name = document.getElementById('name').value;
+var submit = document.getElementById('submit');
+submit.onclick= function(){
+    
+    var names=["name1","name2","name3","name4"];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+= '<li>'+names[i]+'</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
