4ff58025eb2354a398a1b5c8159d50da2152486c
Tuhin Das <tuhin.loves.federer@gmail.com>
2016-11-22 03:10:27 +0530

    Made changes

--- server.js
+++ server.js
@@ -1,50 +1,100 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var http = require("https");
 
 var app = express();
 app.use(morgan('combined'));
 
-
-function createTemplate(data) {
-    var title = data.title;
-    var heading = data.heading;
-    var location = data.location;
-    var content = data.content;
-}
+var api_key = "9c6f4edf8b2c52678bf8e0885ff611d9";
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
+function getSearchRequestOptions(movie_name){
+    var options = {
+      "method": "GET",
+      "hostname": "api.themoviedb.org",
+      "port": null,
+      "path": "/3/search/movie?query="+movie_name+"&language=en-US&api_key="+api_key,
+      "headers": {}
+    };
+    return options;
+}
 
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-  res.send(counter.toString());
+function getMovieDetailsOptions(movie_id){
+    var options = {
+      "method": "GET",
+      "hostname": "api.themoviedb.org",
+      "port": null,
+      "path": "/3/movie/"+movie_id+"?api_key="+api_key,
+      "headers": {}
+    };
+    return options;
+}
+
+app.get('/search-movie', function(req, res){
+	var name = req.query.movie_name;
+	name = name.replace(/ /g, "%20");
+	console.log("search-movie name = "+name);
+
+    var req1 = http.request(getSearchRequestOptions(name), function(res1){
+    
+        var chunks = [];
+        
+        res1.on("data", function(chunk){
+          chunks.push(chunk);
+        });
+        
+        res1.on("end", function(){
+            var body = Buffer.concat(chunks);
+	    body = JSON.parse(body);
+            console.log(body.toString());
+          
+            if(res1.statusCode==200){
+                var movie_id = body["results"][0]["id"];
+                console.log("search result id = "+movie_id);
+                res.redirect("/write-review?movie_id="+movie_id);
+           }
+            else{
+                console.log("ERROR status "+res1.statusCode);
+                res.send("ERROR status "+res1.statusCode);
+            }
+        });
+    });
+
+    req1.write("{}");
+    req1.end();
 });
 
-var http = require("https");
-
-var request = require("request");
-
-var options = { method: 'GET',
-  url: 'https://api.themoviedb.org/3/search/movie',
-  qs: 
-   { query: 'La La Land',
-     language: 'en-US',
-     api_key: '9c6f4edf8b2c52678bf8e0885ff611d9' },
-  body: '{}' };
-
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-
-  console.log(body);
-}); 
-   
-app.get('/:articleName', function (req, res) {
-var articleName = req.params.articleName;
-  res.send(createTemplate(stuff[articleName]));
+app.get('/write-review', function(req, res){
+    var id = req.query.movie_id;
+    id = 6352;
+    console.log("write-review id="+id);
+    
+    var req1 = http.request(getMovieDetailsOptions(id), function(res1){
+        var chunks = [];
+        
+        res1.on("data", function(chunk){
+            chunks.push(chunk);
+        });
+        
+        res1.on("end", function(){
+            var body = Buffer.concat(chunks);
+            console.log(body.toString());
+            
+            if(res1.statusCode==200){
+                res.send("Send an HTML template with filled movie details");
+            }
+            else{
+                console.log("ERROR status "+res1.statusCode);
+                res.send("ERROR status "+res1.statusCode);
+            }
+        });
+    });
+    req1.write("{}");
+    req1.end();
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -92,5 +142,5 @@ app.get('/ui/Harry.jpg', function (req, res) {
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log("IMAD course app listening on port "+port);
 });
