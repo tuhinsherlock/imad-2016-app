96c33905504de2a741a00d14058b52e604c441b5
Tuhin Das <tuhin.loves.federer@gmail.com>
2016-11-22 21:22:04 +0530

    Changes made locally

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var http = require('https');
-var bodyParser=require('body-parser');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var pgdbconfig = {
+    user: 'tuhinsherlock',
+    database: 'tuhinsherlock',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: 'db-tuhinsherlock-17607'
+};
+
+var pool = new Pool(pgdbconfig);
+
+console.log("Connected to DB");
 
 var app = express();
 app.use(morgan('combined'));
-var config;
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
+
+var tmdbconfig;
 
 app.use(bodyParser.json());
 
 var api_key = '9c6f4edf8b2c52678bf8e0885ff611d9';
 
-var configreq = http.request({
+var tmdbconfigreq = http.request({
     "method": "GET",
     "hostname": "api.themoviedb.org",
     "port": null,
-    "path": "/3/configuration?api_key="+api_key,
+    "path": "/3/tmdbconfiguration?api_key="+api_key,
     "headers": {} }, function (res) {
   var chunks = [];
 
@@ -27,13 +47,13 @@ var configreq = http.request({
   res.on("end", function () {
     var body = Buffer.concat(chunks);
     body = JSON.parse(body);
-    config = body;
-    console.log('Config ---> '+JSON.stringify(body));
+    tmdbconfig = body;
+    console.log('tmdbconfig ---> '+JSON.stringify(body));
   });
 });
 
-configreq.write("{}");
-configreq.end();
+tmdbconfigreq.write("{}");
+tmdbconfigreq.end();
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -96,17 +116,16 @@ app.get('/search-movie', function(req, res){
 });
 
 app.post('/submit-review', function(req,res) {
-    console.log(req.body);
+  console.log(req.body);
   var body = req.body;
   var reviewcon = body.reviewcon;
   console.log(reviewcon);
 });
 
-app.get('/write-review', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'write-review.html'));
-});
+
 
 app.get('/get-movie-details', function(req, res){
+
     var id = req.query.movie_id;
 
     console.log("get-movie-details id="+id);
@@ -126,7 +145,7 @@ app.get('/get-movie-details', function(req, res){
             if(res1.statusCode==200){
                 var movie_details = {
                     movie_name : body.title,
-                    poster_path : config['images']['base_url']+config['images']['poster_sizes'][0]+body.poster_path,
+                    poster_path : tmdbconfig['images']['base_url']+tmdbconfig['images']['poster_sizes'][0]+body.poster_path,
                     release_date : body.release_date,
                     overview : body.overview
                 }
@@ -143,16 +162,102 @@ app.get('/get-movie-details', function(req, res){
     req1.end();
 });
 
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'hello-darkness-my-old-friend');
+   res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
+});
+
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.status(403).send('username/password is invalid');
+          } else {
+              // Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+                
+                // Set the session
+                req.session.auth = {userId: result.rows[0].id};
+                // set cookie with a session id
+                // internally, on the server side, it maps the session id to an object
+                // { auth: {userId }}
+                
+                res.send('credentials correct!');
+                
+              } else {
+                res.status(403).send('username/password is invalid');
+              }
+          }
+      }
+   });
+});
+
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
+
+app.get('/logout', function (req, res) {
+   delete req.session.auth;
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+});
+
+app.get('/browse', function(req, res){
+  res.sendFile(path.join(__dirname, 'ui', 'browse.html'));
+});
+
+app.get('/write-review', function(req, res){
+  res.sendFile(path.join(__dirname, 'ui', 'write-review.html'));
+});
+
 app.get('/write-review.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'write-review.js'));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 app.get('/ui/main.js', function (req, res) {
@@ -163,32 +268,10 @@ app.get('/ui/Batman.gif', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Batman.gif'));
 });
 
-app.get('/ui/shawshank.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'shawshank.jpg'));
-});
-
-app.get('/ui/batlogo.gif', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'batlogo.gif'));
-});
 
 app.get('/ui/bootstrap.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css'));
 });
-
-app.get('/ui/Sherlock.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Sherlock.jpg'));
-});
-
-app.get('/ui/FightClub.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'FightClub.jpg'));
-});
-
-app.get('/ui/Harry.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Harry.jpg'));
-});
-
-
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
--- ui/main.js
+++ ui/main.js
@@ -1,52 +1,96 @@
-var button = document.getElementById('counter');
-
-button.onclick = function() {
+function loadLoginForm () {
     
+    // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
+    
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
+
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    window.location.href = "/browse";   
+}
+
+function loadLogin () {
+    // Check if the user is already logged in
     var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function() {
-        
-        if(request.readyState === XMLHttpRequest.DONE)
-        {
-            if(request.status === 200)
-            {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
             }
         }
     };
     
-    request.open('GET','http://tuhinsherlock.imad.hasura-app.io/counter', true);
+    request.open('GET', '/check-login', true);
     request.send(null);
-};
+}
 
-
-var submit = document.getElementById('submit_btn');
-submit.onclick= function(){
-     var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function() {
-        
-        if(request.readyState === XMLHttpRequest.DONE)
-        {
-            if(request.status === 200)
-            {
-                var names= request.responseText;
-                names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {
-        list+= '<li>'+names[i]+'</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-            }
-        }
-    };
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://tuhinsherlock.imad.hasura-app.io/submit-name?name='+name, true);
-    request.send(null);
-    
-};
+// The first thing to do is to check if the user is logged in!
+loadLogin();
