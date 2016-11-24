5832e5e18bda24fa90b3b731e9c6d061dd609dc6
tuhinsherlock <tuhin.loves.federer@gmail.com>
2016-11-25 03:49:10 +0530

    [imad-console] Updates ui/browse.js

--- ui/browse.js
+++ ui/browse.js
@@ -10,15 +10,15 @@ request1.onreadystatechange = function(){
 	if(request1.readyState === XMLHttpRequest.DONE){
 		console.log('DONE '+request1.status);
 		if(request1.status===200){
-			console.log("Success")
+			console.log("Success");
 		}
 		else
 		console.log("Error");
 	}
 
-request1.open('GET','/search-movie?movie_name='+movie_name);
+request1.open('GET','/search-movie?movie_name='+movie_name, true);
 request1.send('{}');
 
 };
 
-}
+};
