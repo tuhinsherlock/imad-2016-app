0030235e5c5a485066c7a9011d1a7391a4478495
tuhinsherlock <tuhin.loves.federer@gmail.com>
2016-11-26 16:25:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -393,6 +393,11 @@ app.get('/ui/bootstrap.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css'));
 });
 
+app.get('/ui/browse.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'browse.css'));
+});
+
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log("IMAD course app listening on port "+port);
